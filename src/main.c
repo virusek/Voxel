39d5a78569fc14fa65b7c794360458efee672a06
virusek <virusek1213@gmail.com>
2025-02-28 22:11:22 +0100

    Basic OpenGL triangle

--- src/main.c
+++ src/main.c
@@ -2,30 +2,107 @@
 
 #include <glad/glad.h>
 #include <stdio.h>
+#include <string.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 outColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   outColor = vec4(0.2, 0.5, 1, 1.0);\n"
+                                   "}\0";
+
+void key_callback(GLFWwindow *window, i32 key, i32 scancode, i32 action,
+                  i32 mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 
 int main(void) {
   if (!glfwInit()) {
     printf("Error: Couldn't initialize GLFW\n");
-
-    const char *description;
-    i32 code = glfwGetError(&description);
-
-    printf("GLFW Error code: %d, %s\n", code, description);
-
     return -1;
   }
 
-  void *window = glfwCreateWindow(640, 480, "Voxel", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(1280, 720, "Voxel", NULL, NULL);
   if (!window) {
     printf("Error: Couldn't create a window!\n");
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // TODO: Change for FPS limiting instead of VSync
+
+  // Callbacks
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // Init OpenGL
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD");
+    return -1;
+  }
+
+  glViewport(0, 0, 1280, 720);
+
+  // Triangle Setup
+
+  f32 vertices[] = {
+      -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f,
+  };
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+
+  u32 vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  u32 vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  u32 fragShader;
+  fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragShader);
+
+  u32 program;
+  program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragShader);
+  glLinkProgram(program);
+  glUseProgram(program);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragShader);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
 
   while (!glfwWindowShouldClose(window)) {
-    // Inf loop
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
   }
 
   glfwDestroyWindow(window);
